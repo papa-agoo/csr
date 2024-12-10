a16d0e1b3d17859680b45a778c91de11b34a84c1
Andreas Gummer <andreas.gummer@gmx.de>
2024-12-10 09:45:27 +0100

    model_viewer: add vertex_format_get_stride()

--- code/applets/main/model_viewer/renderer/common.h
+++ code/applets/main/model_viewer/renderer/common.h
@@ -2,10 +2,11 @@
 
 #pragma once
 
+#include <csr/core/vector.h>
+
 #include <csr/core/math.h>
 #include <csr/core/math/aabb.h>
 #include <csr/core/math/transform.h>
-#include <csr/core/vector.h>
 
 #include <csr/graphics/xgl.h>
 #include <csr/graphics/softgl.h>
@@ -26,6 +27,21 @@ enum vertex_format
     VERTEX_FORMAT_TANGENT_BIT       = BIT(5),
     VERTEX_FORMAT_BITANGENT_BIT     = BIT(6),
 };
+
+CSR_INLINE u32 vertex_format_get_stride(u32 vertex_format)
+{
+    u32 stride = 0;
+
+    stride += (vertex_format & VERTEX_FORMAT_POSITION_BIT)      ? sizeof(struct vec3) : 0;
+    stride += (vertex_format & VERTEX_FORMAT_COLOR_BIT)         ? sizeof(struct vec3) : 0;
+    stride += (vertex_format & VERTEX_FORMAT_NORMAL_BIT)        ? sizeof(struct vec3) : 0;
+    stride += (vertex_format & VERTEX_FORMAT_TEX_COORD0_BIT)    ? sizeof(struct vec2) : 0;
+    stride += (vertex_format & VERTEX_FORMAT_TEX_COORD1_BIT)    ? sizeof(struct vec2) : 0;
+    stride += (vertex_format & VERTEX_FORMAT_TANGENT_BIT)       ? sizeof(struct vec3) : 0;
+    stride += (vertex_format & VERTEX_FORMAT_BITANGENT_BIT)     ? sizeof(struct vec3) : 0;
+
+    return stride;
+}
 
 struct vertex_1p
 {
